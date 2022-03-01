3cb5dd0cd7e72934b2309ad0bbfc7f25a623de82
Your NameCszola23 <cszola23@hotmail.com>
2022-03-01 19:52:42 +0100

    Első feltöltés

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,15 @@
+import './ExpenseItem.css';
+
+function ExpenseItem() {
+  return (
+    <div>
+      <div>March 28th 2021</div>
+      <div>
+        <h2>Car Insurance</h2>
+        <div>123.45$</div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
