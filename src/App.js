08907899ad91199910213cb7e3f6d372387e7287
Your NameCszola23 <cszola23@hotmail.com>
2022-03-06 18:38:45 +0100

    amit eredetileg is kellett volna

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+  const expenses = [
+    {
+      title: "Car Insurance",
+      amount: 123.45,
+      date: new Date(2021, 2, 11),
+    },
+    { title: "Chicken breast", amount: 100, date: new Date(2022, 1, 10) },
+    { title: "RTX 3060", amount: 705, date: new Date(2022, 1, 11) },
+  ];
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem></ExpenseItem>
+      <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}
+      ></ExpenseItem>
     </div>
   );
 }
